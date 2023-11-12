0c4c1abbb0e915ba57050c5b418a8e38beaaee42
Bilal-Yucell <bilallyucell@gmail.com>
2023-11-12 19:59:14 +0300

    Updated Git Configuration

--- Assets/Scripts/Runtime/Extensions/LockCinemachineAxis.cs
+++ Assets/Scripts/Runtime/Extensions/LockCinemachineAxis.cs
@@ -1,56 +1,25 @@
-using System;
 using Cinemachine;
 using UnityEngine;
 
 namespace Runtime.Extensions
 {
-    public enum CinemachineLockAxis
-    {
-        X,
-        Y,
-        Z
-    }
-    
     [ExecuteInEditMode]
     [SaveDuringPlay]
     [AddComponentMenu("")]
-    public class LockCinemachineAxis : CinemachineExtension
+    public class LookCinemachineAxis : CinemachineExtension
     {
-        [SerializeField] private CinemachineLockAxis lockAxis;
-        [Tooltip("Lock The Cinemachine Virtual Camera's X Axis position with this spescfic value")]
-        public float XClampValue = 0;
-        
-        protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, 
+        [Tooltip("Lock the camera's X position to this value")]
+        public float m_XPosition = 0f;
+
+        protected override void PostPipelineStageCallback(
+            CinemachineVirtualCameraBase vcam,
             CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
         {
-            switch (lockAxis)
+            if (stage == CinemachineCore.Stage.Body)
             {
-                case CinemachineLockAxis.X:
-                    if (stage == CinemachineCore.Stage.Body)
-                    {
-                        var pos = state.RawPosition;
-                        pos.x = XClampValue;
-                        state.RawPosition = pos;
-                    }
-                    break;
-                case CinemachineLockAxis.Y:
-                    if (stage == CinemachineCore.Stage.Body)
-                    {
-                        var pos = state.RawPosition;
-                        pos.y = XClampValue;
-                        state.RawPosition = pos;
-                    }
-                    break;
-                case CinemachineLockAxis.Z:
-                    if (stage == CinemachineCore.Stage.Body)
-                    {
-                        var pos = state.RawPosition;
-                        pos.z = XClampValue;
-                        state.RawPosition = pos;
-                    }
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
+                var pos = state.RawPosition;
+                pos.x = m_XPosition;
+                state.RawPosition = pos;
             }
         }
     }
